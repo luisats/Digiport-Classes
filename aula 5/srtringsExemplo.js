d8b571d76f4deabd92487242dffa2bc8cb8b8914
Luisa Soares <luisatsoares@gmail.com>
2025-07-21 15:31:21 -0300

    Aula 5

--- aula 5/srtringsExemplo.js
+++ aula 5/srtringsExemplo.js
@@ -1,23 +1,11 @@
-// Crie uma variável string "Javascript é incrível"
+
 let umaString = "Javascript é incrível";
-
-// Acesse e imprima a 5ª letra da string
-console.log(umaString[4]); // Retorna "s"
-
-// Use charAt() para acessar o mesmo caractere
-console.log(umaString.charAt(4)); // Retorna "s"
-
-// Substitua a palavra "incrível" por "poderoso"
+console.log(umaString[4]); 
+console.log(umaString.charAt(4)); 
 let novaString = umaString.replace("incrível", "poderoso");
-console.log(novaString); // Retorna "Javascript é poderoso"
-
-// Imprima o número de caracteres da string
-console.log(umaString.length); // Retorna 20
-
-// Use slice() para extrair a palavra "Javascript"
+console.log(novaString); 
+console.log(umaString.length); 
 let palavraExtraida = umaString.slice(0, 10);
-console.log(palavraExtraida); // Retorna "Javascript"
-
-// Use concat() para adicionar "Vamos aprender!" ao final da string
+console.log(palavraExtraida);
 let stringConcatenada = umaString.concat(" Vamos aprender!");
-console.log(stringConcatenada); // Retorna "Javascript é incrível Vamos aprender!"
+console.log(stringConcatenada); 
